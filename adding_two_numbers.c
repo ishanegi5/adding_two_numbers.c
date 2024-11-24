7be96d617ca1ad8f989e8dcae4d17826d1e5f2f5
Isha Negi <itsishanegi@gmail.com>
2024-11-24 12:32:59 +0530

    Create adding_two_numbers.c

---
+++ adding_two_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int add(int x,int y)
+{   int sum=x+y;
+    return sum;
+}
+int main()
+{ int a=20;
+   int b=30;
+   printf("sum= %d",add(a,b));
+   printf("\nName: Isha Negi \nRoll number: 24 CE 47\n");
+    return 0;
+}
